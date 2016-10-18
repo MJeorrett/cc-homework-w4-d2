eb68b0f633b7431ed012a5e40efe54b4c99e7856
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-18 18:47:20 +0100

    fixing minor bug

--- controller.rb
+++ controller.rb
@@ -4,5 +4,5 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 
 get '/:table_name' do
-  return "I am not yet sure if I the table '#{table_name}' exists in my database"
+  return "I am not yet sure if I the table '#{params[:table_name]}' exists in my database"
 end
